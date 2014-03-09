d7cdd95bda9fc83b1d4485f6b77388c69d961e3d
Mihai <mihai.bulaicon@yahoo.com>
2014-03-09 19:18:22 +0200

    sectii

---
+++ CloudStorage/Scripts/AngularControllers/sectii.js
@@ -0,0 +1,53 @@
+﻿angular.module('CloudStorage').controller('sectii', ['$scope', function ($scope) {
+    $scope.myData = [];
+    $scope.refresh = function () {
+        $.ajax({
+            url: "/CloudStorage/api/sectii",
+            dataType: 'json',
+            async: false,
+            success: function (data) {
+                $scope.myData = data;
+            }
+        });
+        if (!$scope.$$phase)
+            $scope.$apply();
+    }
+
+    $scope.refresh();
+
+    $scope.gridOptions = {
+        data: 'myData',
+        enableCellSelection: true,
+        enableRowSelection: false,
+        enableCellEditOnFocus: true,
+        columnDefs: [{ field: 'Nume', displayName: 'Nume', enableCellEdit: true },
+                     { field: 'deleteButton', displayName: 'Sterge', width: 90, cellTemplate: '<button ng-click="removeRow(row)">Șterge</button>', enableCellEdit: false }]
+    };
+
+
+    $scope.SaveChanges = function () {
+        $.ajax({
+            type: "PUT",
+            url: "/CloudStorage/api/sectii/",
+            data: JSON.stringify($scope.myData),
+            dataType: 'json',
+            contentType: "application/json"
+        });
+        $.getJSON("/CloudStorage/api/sectii", function (data) {
+            $scope.myData = data;
+        });
+    };
+    $scope.removeRow = function (row) {
+        var index = $scope.myData.indexOf(row.entity);
+        $.ajax({
+            type: "DELETE",
+            url: "/CloudStorage/api/sectii/" + row.entity.Id,
+        });
+        $scope.gridOptions.selectItem(index, false);
+        $scope.myData.splice(index, 1);
+
+    };
+    $scope.addRow = function () {
+        $scope.myData.push({ Nume: '' });
+    };
+}]);
